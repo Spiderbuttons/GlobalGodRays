877b9f9512eea699a25593c6c8fdc68ec1442b4b
Spiderbuttons <azurylldmm@gmail.com>
2025-12-17 21:07:25 -0500

    More config options.

--- Config/ModConfig.cs
+++ Config/ModConfig.cs
@@ -5,9 +5,12 @@ namespace GlobalGodRays.Config;
 
 public sealed class ModConfig
 {
+    public string RayStyle { get; set; } = "Vanilla";
     public float RayScale { get; set; } = 0.25f;
     public float RayIntensity { get; set; } = 4f;
     public float RayAnimationSpeed { get; set; } = 20f;
+    
+    public float RayOpacityModifier { get; set; } = 1f;
 
     public ModConfig()
     {
@@ -16,9 +19,11 @@ public sealed class ModConfig
 
     private void Init()
     {
-        RayScale = 0.25f;
+        RayStyle = "Vanilla";
+        RayScale = 0.65f;
         RayIntensity = 4f;
         RayAnimationSpeed = 20f;
+        RayOpacityModifier = 1f;
     }
 
     public void SetupConfig(IGenericModConfigMenuApi configMenu, IManifest ModManifest, IModHelper Helper)
@@ -27,6 +32,20 @@ public sealed class ModConfig
             mod: ModManifest,
             reset: Init,
             save: () => Helper.WriteConfig(this)
+        );
+        
+        configMenu.AddTextOption(
+            mod: ModManifest,
+            name: () => "Ray Style",
+            tooltip: () => "Changes the texture used for drawing the godrays. If you change this, you will likely want to adjust the Ray Scale setting as well.",
+            getValue: () => RayStyle,
+            setValue: value =>
+            {
+                RayStyle = value;
+                RayScale = value is "Vanilla" ? 0.65f : 0.25f;
+                ModEntry.RayManager?.UpdateValues(null, null);
+            },
+            allowedValues: ["Vanilla", "HiRes"]
         );
 
         configMenu.AddNumberOption(
@@ -37,8 +56,13 @@ public sealed class ModConfig
             setValue: value => RayScale = value,
             min: 0.1f,
             max: 3.0f,
-            interval: 0.05f
-        );
+            interval: 0.05f,
+            formatValue: value =>
+            {
+                float baseScale = RayStyle is "Vanilla" ? 0.65f : 0.25f;
+                float percentage = value / baseScale * 100f;
+                return $"{percentage:0}%";
+            });
 
         configMenu.AddNumberOption(
             mod: ModManifest,
@@ -49,8 +73,13 @@ public sealed class ModConfig
             setValue: value => RayIntensity = value,
             min: 1f,
             max: 32f,
-            interval: 1f
-        );
+            interval: 1f,
+            formatValue: value =>
+            {
+                float baseIntensity = 4f;
+                float percentage = value / baseIntensity * 100f;
+                return $"{percentage:0}%";
+            });
         
         configMenu.AddNumberOption(
             mod: ModManifest,
@@ -60,7 +89,28 @@ public sealed class ModConfig
             setValue: value => RayAnimationSpeed = value,
             min: 1f,
             max: 100f,
-            interval: 1f
+            interval: 1f,
+            formatValue: value =>
+            {
+                float baseSpeed = 20f;
+                float percentage = value / baseSpeed * 100f;
+                return $"{percentage:0}%";
+            });
+        
+        configMenu.AddNumberOption(
+            mod: ModManifest,
+            name: () => "Ray Opacity Multiplier",
+            tooltip: () => "Adjusts the overall opacity of the godrays. Higher values make them more opaque. Lower values make them more transparent.",
+            getValue: () => RayOpacityModifier,
+            setValue: value => RayOpacityModifier = value,
+            min: 0.1f,
+            max: 3f,
+            interval: 0.1f,
+            formatValue: value =>
+            {
+                float percentage = value * 100f;
+                return $"{percentage:0}%";
+            }
         );
     }
 }
