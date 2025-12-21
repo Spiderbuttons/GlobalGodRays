40588fa18c6f4919713d645a636a49a35721e83c
Spiderbuttons <azurylldmm@gmail.com>
2025-12-21 01:50:55 -0500

    Remove Harmony stuff.

--- Helpers/Log.cs
+++ Helpers/Log.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Diagnostics;
 using StardewModdingAPI;
-using System.Linq;
-using HarmonyLib;
 
 // ReSharper disable PossibleMultipleEnumeration
 // ReSharper disable once RedundantArgumentDefaultValue
@@ -37,36 +35,6 @@ public static class Log
     public static void Alert<T>(T message) => ModEntry.ModMonitor.Log(
         $"{(message is not string ? "[" + message?.GetType() + "] " : string.Empty)}{message?.ToString() ?? string.Empty}",
         LogLevel.Alert);
-
-    [Conditional("DEBUG")]
-    public static void ILCode(IEnumerable<CodeInstruction> newCode, IEnumerable<CodeInstruction> originalCode)
-    {
-        var originalEnumerator = 0;
-        foreach (var instruction in newCode)
-        {
-            if (originalEnumerator >= originalCode.Count())
-            {
-                Warn($"{instruction.opcode} {instruction.operand}");
-                continue;
-            }
-
-            if (instruction.opcode != originalCode.ElementAt(originalEnumerator).opcode ||
-                instruction.operand != originalCode.ElementAt(originalEnumerator).operand)
-            {
-                Warn($"{instruction.opcode} {instruction.operand}");
-                continue;
-            }
-
-            Debug($"{instruction.opcode} {instruction.operand}");
-            originalEnumerator++;
-        }
-    }
-
-    [Conditional("DEBUG")]
-    public static void ILCode(CodeInstruction code)
-    {
-        Debug($"{code.opcode} {code.operand}");
-    }
 
     [Conditional("DEBUG")]
     public static void LogPairs(this IEnumerable? enumerable, int depth = 0)
