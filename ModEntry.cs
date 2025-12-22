969543e555f455e359c53e13471d7085bca59aa5
Spiderbuttons <azurylldmm@gmail.com>
2025-12-22 13:44:48 -0500

    Final cleanup.

--- ModEntry.cs
+++ ModEntry.cs
@@ -1,31 +1,25 @@
-﻿using System.Text;
-using GenericModConfigMenu;
+﻿using GenericModConfigMenu;
 using GlobalGodRays.Config;
-using GlobalGodRays.Helpers;
 using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
-using StardewValley;
 
 namespace GlobalGodRays
 {
     internal sealed class ModEntry : Mod
     {
-        internal static IModHelper ModHelper { get; set; } = null!;
-        internal static IMonitor ModMonitor { get; set; } = null!;
-        internal static ModConfig Config { get; set; } = null!;
-        
-        public static RayManager? RayManager { get; set; }
+        internal static IModHelper ModHelper { get; private set; } = null!;
+        internal static ModConfig Config { get; private set; } = null!;
+
+        private static RayManager? RayManager { get; set; }
 
         public override void Entry(IModHelper helper)
         {
             i18n.Init(helper.Translation);
             ModHelper = helper;
-            ModMonitor = Monitor;
             Config = helper.ReadConfig<ModConfig>();
 
             Helper.Events.Content.AssetRequested += OnAssetRequested;
-            Helper.Events.Input.ButtonPressed += OnButtonPressed;
             Helper.Events.GameLoop.GameLaunched += OnGameLaunched;
             Helper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
             Helper.Events.GameLoop.ReturnedToTitle += OnReturnedToTitle;
@@ -54,23 +48,6 @@ namespace GlobalGodRays
         {
             var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
             if (configMenu != null) Config.SetupConfig(configMenu, ModManifest, Helper);
-        }
-
-        private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
-        {
-            if (!Context.IsWorldReady)
-                return;
-
-            if (e.Button is SButton.F3)
-            {
-                RayManager?.Dispose();
-                RayManager = new RayManager();
-            }
-
-            if (e.Button is SButton.F5)
-            {
-                for (int i = 0; i < 50; i++) Game1.currentLocation.addClouds(1, true);
-            }
         }
     }
 }
